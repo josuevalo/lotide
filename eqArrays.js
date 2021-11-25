6ec9ae374b7d397f1c9eefd3f1d2431b926581cb
Josue <arevalo.josued@gmail.com>
2021-11-25 05:24:32 +0000

    eqObjects.js

--- eqArrays.js
+++ eqArrays.js
@@ -7,6 +7,9 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function(array1, array2) {
   let value = true;
+  if (array1.length !== array2.length){
+    value = false
+  }
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] !== array2[i]) {
       value = false;
